b32360a93e0c9f64efd3798d2159085667e0b46e
Owen-kasule <owenatug@gmail.com>
2025-07-09 22:20:11 +0300

    This base programe

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import ProductPage from './components/ProductPage';
+import SellerProfile from './components/SellerProfile';
+import AboutPage from './components/AboutPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/seller/:id" element={<SellerProfile />} />
+            <Route path="/products" element={<div className="placeholder">Products Page - Coming Soon</div>} />
+            <Route path="/sellers" element={<div className="placeholder">Sellers Page - Coming Soon</div>} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/login" element={<div className="placeholder">Login Page - Coming Soon</div>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
